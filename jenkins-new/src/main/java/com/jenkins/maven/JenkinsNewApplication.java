f62bc92d1a094ce355955737c2074dda84e7a6b5
suraj <sksuraj01@gmail.com>
2023-09-09 12:34:01 +0530

    new

---
+++ jenkins-new/src/main/java/com/jenkins/maven/JenkinsNewApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsNewApplication.class, args);
+	}
+
+}
